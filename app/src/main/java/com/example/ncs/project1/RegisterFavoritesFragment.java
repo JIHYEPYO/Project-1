0bdd2f72cbbe4ac90e1ac3d5a616a1553106b32f
pyojihye <pyojihye95@gmail.com>
2017-05-16 16:23:21 +0900

    datepicker

--- app/src/main/java/com/example/ncs/project1/RegisterFavoritesFragment.java
+++ app/src/main/java/com/example/ncs/project1/RegisterFavoritesFragment.java
@@ -1,12 +1,21 @@
 package com.example.ncs.project1;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by NCS on 2017-05-16.
@@ -32,11 +41,38 @@ public class RegisterFavoritesFragment extends android.support.v4.app.Fragment {
         buttonRegisterDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                DialogFragment dFragment = new DatePickerFragment();
+                dFragment.show(getFragmentManager(), "Date Picker");
             }
         });
 
         return v;
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dpd = new DatePickerDialog(getActivity(),
+                    AlertDialog.THEME_HOLO_LIGHT,this,year,month,day);
+
+            // Return the DatePickerDialog
+            return  dpd;
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day){
+            // Do something with the chosen date
+        }
+
+        public void onCancel(DialogInterface dialog){
+            Toast.makeText(getActivity(),"Date Picker Canceled.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public static synchronized RegisterFavoritesFragment getInstance() {
